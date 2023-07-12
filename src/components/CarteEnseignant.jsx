5d852fe23837b9ca1155bfeac021b1c04eccae51
wassim-mouloud <simo.rko10@gmail.com>
2023-07-12 12:09:59 +0100

    fixing galerie  and enseignants

--- src/components/CarteEnseignant.jsx
+++ src/components/CarteEnseignant.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CarteEnseignant({nom, matiere}) {
   return (
-    <div>
+    <div className='flex flex-col items-center ' >
         <div className='relative h-[280px] w-[230px] rounded-[40px] border-[3px] border-[#306DC9] border-solid ' >
             <img src="./images/skender.png" alt="" className='absolute bottom-0 left-[50%] translate-x-[-50%] ' />
         </div>
--- src/components/Enseignants.jsx
+++ src/components/Enseignants.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import CarteEnseignant from './CarteEnseignant'
+import Galerie from './Galerie'
 function Enseignants() {
 
-    const teachers=[0,1,2,3,4,5,6,7,8,9,10]
+    const teachers=[0,1,2,3,4,5,6,7,8,9,10,11]
 
   return (
     <div className='flex flex-col items-center gap-[100px]  ' >
         <p className='text-black font-bold text-[28px] lg:text-[40px] p-5 lg:p-8' >Nos enseignants</p>
-        <div className='grid grid-cols-1 gap-[100px] lg:gap-x-[50px] lg:gap-y-[100px] lg:grid-cols-4' >
+        <div className='grid grid-cols-1 gap-[100px] lg:gap-x-[50px] lg:gap-y-[100px] lg:grid-cols-4  ' >
             {teachers.map((teacher, i)=>{
                 return(
                    <CarteEnseignant key={i}  nom='Skander' matiere='Philosophie'/>
                 )
             })}
-       
         </div>
     </div>
   )
