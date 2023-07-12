706b7d04a234f47f89b78fa6b1614f01b4c9dfcb
wassim-mouloud <simo.rko10@gmail.com>
2023-07-12 12:49:52 +0100

    fixing hamza bugs

--- src/components/CarteEnseignant.jsx
+++ src/components/CarteEnseignant.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CarteEnseignant({nom, matiere}) {
   return (
     <div className='flex flex-col items-center ' >
-        <div className='relative h-[280px] w-[230px] rounded-[40px] border-[3px] border-[#306DC9] border-solid ' >
+        <div className='relative h-[280px] w-[230px] md:w-[200px] lg:w-[230px] rounded-[40px] border-[3px] border-[#306DC9] border-solid ' >
             <img src="./images/skender.png" alt="" className='absolute bottom-0 left-[50%] translate-x-[-50%] ' />
         </div>
         <p className='font-semibold text-center text-black' >{nom}</p>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,45 +3,27 @@ import React, { useState } from 'react'
 function Hero() {
 
 
-    const [b1Hovered, setb1Hovered] = useState(false);
-    const [b2Hovered, setb2Hovered] = useState(false);
 
-    const handleMouseEnter = () => {
-      setb1Hovered(true);
-    };
-  
-    const handleMouseLeave = () => {
-      setb1Hovered(false);
-    };
-
-
-    const handleMouseEnter2 = () => {
-        setb2Hovered(true);
-      };
-    
-      const handleMouseLeave2 = () => {
-        setb2Hovered(false);
-      };
 
   return (
-    <div className='flex flex-col items-center justify-center gap-8' >
-        <div className='flex flex-col lg:flex-row lg:justify-between  w-screen lg:w-[80vw]   lg:mx-auto gap-6 p-6 lg:p-0 ' >
+    <div className='flex flex-col items-center justify-center gap-8 lg:mb-[30px] lg:mt-[30px]' >
+        <div className='flex flex-col lg:flex-row lg:justify-between  w-screen lg:w-[80vw] lg:mx-auto gap-6 p-6 lg:p-0 ' >
             <div className='flex flex-col items-center gap-6 lg:items-start ' >
-                <h1 className='text-black text-center lg:text-left font-bold text-[28px] lg:text-[50px] ' >
+                <h1 className='text-black text-center lg:text-left font-bold text-[28px] md:text-[40px] lg:text-[50px] ' >
                     Première plateforme de <span className='text-[#306DC9]' >financement</span> pour les startups en Algérie
                 </h1>
-                <p className='text-center  lg:text-left text-[#808080] text-[12px] lg:text-[20px] font-semibold w-[80%]' >Démarrez et gérez une collecte de fonds professionnelle
+                <p className='text-center  lg:text-left text-[#808080] text-[12px] md:text-[16px] lg:text-[20px] font-semibold w-[80%]' >Démarrez et gérez une collecte de fonds professionnelle
                     pour attirer des investisseurs qualifiés et accrédités de qualité.
                 </p>
-                <div className='flex gap-6 lg:gap-8' >
+                <div className='flex gap-6 lg:gap-8 md:gap-14' >
                     <button className={`b1 font-semibold text-[12px] text-white lg:text-[14px]  hover:text-[#306DC9] flex justify-center items-center w-[160px] lg:w-[190px] h-[45px] bg-[#306DC9] hover:bg-white rounded-md transition-all duration-300 border border-solid   hover:border-[#306DC9]  `} >Reserver maintenant</button>
                     <button className={`b2 font-semibold text-[12px] lg:text-[14px] text-[#306DC9] hover:text-white flex justify-center items-center w-[160px] lg:w-[190px] h-[45px] bg-white hover:bg-[#306DC9] rounded-md border-[1px] border-solid border-[#306DC9] hover:border-white transition-all duration-300 `} >Voir le planning</button>
                 </div>
             </div>
-            <img src="./images/classe.png" alt="rocket launch" className='w-[full] lg:w-[462px] lg:h-[462px]' />
+            <img src="./images/classe.png" alt="rocket launch" className='w-[full] my-auto md:w-full md:h-[500px]  lg:w-[462px] lg:h-[462px]' />
         </div>
-        <div className='flex w-full lg:w-[80%] items-center justify-between lg:justify-center gap-3 lg:gap-[200px] mx-auto p-6 lg:p-0 ' >
-            <div className='items-center justify-center hidden gap-4 lg:flex'>
+        <div className='flex w-full md:w-[80%] lg:w-[80%] items-center justify-between lg:justify-center gap-3 lg:gap-[200px] mx-auto p-6 lg:p-0 ' >
+            <div className='items-center justify-center hidden gap-4 md:flex'>
                 <img src="./images/enseignant.png" alt="" className='h-[30px] w-[30px]'/>
                 <div>
                     <p className='text-[#306DC9] font-bold block' >+32</p>
