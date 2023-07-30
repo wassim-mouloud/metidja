5bd5b7b5a3df4ccf07a95818fa9a0a32004d5268
wassim-mouloud <simo.rko10@gmail.com>
2023-07-30 16:49:07 +0100

    fixing slider bugs

--- src/components/DaySlider.jsx
+++ src/components/DaySlider.jsx
@@ -19,19 +19,23 @@ function DaySlider({day}) {
         sliderRef.current.style.transition = 'transform 0.5s linear';
         sliderRef.current.style.transform = 'translateX(' + (size * newIndex) + 'px)';    }
   return (
-    <div className='w-screen pl-4' >
+    <div className='w-screen px-4' >
         <p className='text-[24px] text-black font-semibold' >{day}</p>
         <div className='relative w-full overflow-hidden' >
-            <div onClick={handlePrev} className='z-[99] h-full w-[40px] bg-[rgba(0,0,0,0.2)] hover:bg-[rgba(0,0,0,0.5)] transition-all duration-200 absolute left-0 top-0 hidden lg:block rounded-l-md' ></div>
+            <div onClick={handlePrev} className='z-[99] h-full w-[40px] cursor-pointer bg-[rgba(0,0,0,0.1)] hover:bg-[rgba(0,0,0,0.5)] transition-all duration-200 absolute left-0 top-0 hidden lg:flex justify-center items-center rounded-l-md' >
+                <img src="/images/left-chevron.png" alt="" className='w-7 h-7' />
+            </div>
             <div ref={sliderRef} className='flex w-full gap-2 pb-4 overflow-x-scroll lg:pb-0 lg:overflow-x-visible' >
                         {cartes.map((carte, index)=>{
                             return(
-                                <div key={index} className={`flex-shrink-0 bg-[#306DC9] rounded-md h-[150px] w-[calc(100%/4-8px)]`} >{index}</div>
+                                <div key={index} className={`flex-shrink-0 bg-[#306DC9] rounded-md h-[150px] ${index%4===3?'w-[calc(100%/4)]':'w-[calc(100%/4-8px)]'}`} >{index}</div>
                             )
                         })}
 
             </div>
-            <div onClick={handleNext} className='z-[99] h-full w-[40px] bg-[rgba(0,0,0,0.2)] hover:bg-[rgba(0,0,0,0.5)] transition-all duration-200 absolute right-0 top-0 hidden lg:flex ' ></div>
+            <div onClick={handleNext} className='z-[99] h-full cursor-pointer w-[40px] bg-[rgba(0,0,0,0.1)] hover:bg-[rgba(0,0,0,0.5)] transition-all duration-200 absolute right-0 top-0 hidden lg:flex justify-center items-center rounded-r-md' >
+                <img src="/images/right-chevron.png" alt="" className='w-7 h-7' />
+            </div>
 
         </div>
     </div>
