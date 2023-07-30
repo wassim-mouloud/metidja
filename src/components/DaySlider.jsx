abf4675de43c4aa7deb2f1a8803f07df080e970b
wassim-mouloud <simo.rko10@gmail.com>
2023-07-30 16:15:45 +0100

    finishing slider on pc

--- src/components/DaySlider.jsx
+++ src/components/DaySlider.jsx
@@ -1,19 +1,37 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 function DaySlider({day}) {
-    const [cartes, setCartes]= useState([0,1,2,3,4,5,6,7,8])
+    const [cartes, setCartes]= useState([0,1,2,3,4,5,6,7,8,9,10,11])
+    const [index, setIndex]= useState(0)
+    const sliderRef= useRef(null)
+
+    const handlePrev= ()=>{
+        let size = sliderRef.current.offsetWidth;
+        let newIndex = index === 0 ? -2 : index + 1;
+        setIndex(newIndex);        
+        sliderRef.current.style.transition = 'transform 0.5s linear';
+        sliderRef.current.style.transform = 'translateX(' + (size * newIndex) + 'px)';
+    }
+    const handleNext= ()=>{
+        let size = sliderRef.current.offsetWidth;
+        let newIndex = index === -2 ? 0 : index - 1;
+        setIndex(newIndex);
+        sliderRef.current.style.transition = 'transform 0.5s linear';
+        sliderRef.current.style.transform = 'translateX(' + (size * newIndex) + 'px)';    }
   return (
     <div className='w-screen pl-4' >
         <p className='text-[24px] text-black font-semibold' >{day}</p>
-        <div className='w-full' >
-            <div className='flex w-full gap-2 overflow-x-scroll' >
+        <div className='relative w-full overflow-hidden' >
+            <div onClick={handlePrev} className='z-[99] h-full w-[40px] bg-[rgba(0,0,0,0.2)] hover:bg-[rgba(0,0,0,0.5)] transition-all duration-200 absolute left-0 top-0 hidden lg:block rounded-l-md' ></div>
+            <div ref={sliderRef} className='flex w-full gap-2 pb-4 overflow-x-scroll lg:pb-0 lg:overflow-x-visible' >
                         {cartes.map((carte, index)=>{
                             return(
-                                <div key={index} className='flex-shrink-0 bg-[#306DC9] rounded-md h-[150px] w-[calc(100%/3-12px)]' ></div>
+                                <div key={index} className={`flex-shrink-0 bg-[#306DC9] rounded-md h-[150px] w-[calc(100%/4-8px)]`} >{index}</div>
                             )
                         })}
 
             </div>
+            <div onClick={handleNext} className='z-[99] h-full w-[40px] bg-[rgba(0,0,0,0.2)] hover:bg-[rgba(0,0,0,0.5)] transition-all duration-200 absolute right-0 top-0 hidden lg:flex ' ></div>
 
         </div>
     </div>
--- src/components/Programme.jsx
+++ src/components/Programme.jsx
@@ -3,7 +3,7 @@ import DaySlider from './DaySlider'
 
 function Programme() {
   return (
-    <div className='flex flex-col w-screen mx-auto gap-[100px] ' >
+    <div className='flex flex-col w-screen gap-10 mx-auto ' >
         <p className='text-black font-bold text-[28px] lg:text-[40px] p-5 lg:p-8 mx-auto' >Programme </p>
         <DaySlider day='Dimanche' />
 
